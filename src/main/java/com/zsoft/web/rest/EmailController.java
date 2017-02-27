775d215ce764a75dbe1088ca6b90e595de0463cb
Billel Boudjit <billel.boudjit@zsoft-consulting.com>
2017-02-27 12:25:52 +0100

    Création du calendrier avec la désactivation des week-ends

--- src/main/java/com/zsoft/web/rest/EmailController.java
+++ src/main/java/com/zsoft/web/rest/EmailController.java
@@ -1,17 +1,20 @@
-package com.zsoft.controller;
+package com.zsoft.web.rest;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+
+import static org.springframework.http.MediaType.*;
 
 /**
  * Created by Billel Boudjit on 21/02/2017.
  */
 @RestController
 public class EmailController {
-    public boolean exist = false;
-    @RequestMapping("/emails")
+    @RequestMapping(value="/emails",
+        produces={APPLICATION_JSON_VALUE, TEXT_PLAIN_VALUE})
     public ArrayList<String> emailList()    {
         ArrayList<String> emails = new ArrayList<String>();
         emails.add("belkacem@gmail.com");
