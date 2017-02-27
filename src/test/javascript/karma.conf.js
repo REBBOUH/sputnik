b050f1b7842827ccc90aa3bc9368f15107ba7869
Billel Boudjit <billel.boudjit@zsoft-consulting.com>
2017-02-27 13:20:22 +0100

    Fixer le calendar

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/messageformat/messageformat.js',
             'src/main/webapp/bower_components/sockjs-client/dist/sockjs.js',
             'src/main/webapp/bower_components/stomp-websocket/lib/stomp.min.js',
+            'src/main/webapp/bower_components/moment/moment.js',
             'src/main/webapp/bower_components/angular/angular.js',
             'src/main/webapp/bower_components/angular-aria/angular-aria.js',
             'src/main/webapp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
